4183bde5bc59d327c0bb5eaad66f919d667031a8
Kevin Lee <kevin.code@kevinlee.io>
2017-05-06 01:45:34 +1000

    Closes #31 - String utility with uncapitalize

---
+++ src/main/scala/io/kevinlee/skala/strings/RichString.scala
@@ -0,0 +1,54 @@
+package io.kevinlee.skala.strings
+
+/**
+  * @author Kevin Lee
+  * @since 2017-05-06
+  */
+object RichString {
+  /**
+    * Uncapitalize String.
+    * {{{
+    *   e.g.)
+    *   RichString.uncapitalize("Test")
+    *   // "test"
+    *
+    *   RichString.uncapitalize("TestSomething")
+    *   // "testSomething"
+    *
+    *   RichString.uncapitalize("")
+    *   // ""
+    *
+    *   val a: String = null
+    *   RichString.uncapitalize(a)
+    *   // java.lang.NullPointerException
+    * }}}
+    * @param value
+    * @return
+    */
+  def uncapitalize(value: String): String =
+    if (value.isEmpty) value
+    else value.head.toLower + value.tail
+
+  implicit class ToUncapitalize(val value: String) extends AnyVal {
+    /**
+      * Uncapitalize String
+      * {{{
+      *   e.g.)
+      *   "Test".uncapitalize
+      *   // "test"
+      *
+      *   "TestSomething".uncapitalize
+      *   // "testSomething"
+      *
+      *   "".uncapitalize
+      *   // ""
+      *
+      *   val a: String = null
+      *   a.uncapitalize
+      *   // java.lang.NullPointerException
+      * }}}
+      * @return
+      */
+    def uncapitalize: String = RichString.uncapitalize(value)
+  }
+}

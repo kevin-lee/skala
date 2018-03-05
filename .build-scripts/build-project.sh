8e13b89099a7bfbae23065746ccdd95d0d786494
Kevin Lee <kevin.code@kevinlee.io>
2018-03-06 03:23:01 +1100

    Closes #40 - Cross build for Scala 2.11 and 2.12 - Libraries are too old to support 2.12 => libraries were now up to date - Some plugin versions are too old => plugin versions are now up to date (except sbt-github-release for now) - Test code using ScalaMock is broken for Scala 2.12 => tests for 2.11 and 2.12 were separated and errors in 2.12 version were all fixed. - Build scripts have been updated with cross-build options (e.g. +publish +test +package, etc.)

--- .build-scripts/build-project.sh
+++ .build-scripts/build-project.sh
@@ -5,25 +5,25 @@ echo "Build projects"
 echo "--------------------------------------------"
 echo ""
 echo "============================================"
-echo "Run: sbt clean coverage test package"
+echo "Run: sbt clean +coverage +test +coverageReport +coverageAggregate +package"
 echo "--------------------------------------------"
 if [[ "$BRANCH_NAME" == "rc" ]]
   then
-  if sbt clean coverage test packageBin packageSrc packageDoc ; then
-    echo "Done: sbt clean coverage test packageBin packageSrc packageDoc"
+  if sbt clean +coverage +test +coverageReport +coverageAggregate +packageBin +packageSrc +packageDoc ; then
+    echo "Done: sbt clean +coverage +test +coverageReport +coverageAggregate +packageBin +packageSrc +packageDoc"
     echo "============================================"
   else
-    echo "Failed: sbt clean coverage test packageBin packageSrc packageDoc" 1>&2
+    echo "Failed: sbt clean +coverage +test +coverageReport +coverageAggregate +packageBin +packageSrc +packageDoc" 1>&2
     echo "============================================"
     exit 1
   fi
 fi
 
-if sbt clean coverage test package ; then
-  echo "Done: sbt clean coverage test package"
+if sbt clean +coverage +test +coverageReport +coverageAggregate +package ; then
+  echo "Done: sbt clean +coverage +test +coverageReport +coverageAggregate +package"
   echo "============================================"
 else
-  echo "Failed: sbt clean coverage test package" 1>&2
+  echo "Failed: sbt clean +coverage +test +coverageReport +coverageAggregate +package" 1>&2
   echo "============================================"
   exit 1
 fi
@@ -31,13 +31,13 @@ fi
 echo ""
 
 echo "============================================"
-echo "Run: sbt coveralls"
+echo "Run: sbt +coveralls"
 echo "--------------------------------------------"
-if sbt coveralls ; then
-  echo "Done: sbt coveralls"
+if sbt +coveralls ; then
+  echo "Done: sbt +coveralls"
   echo "============================================"
 else
-  echo "Failed: sbt coveralls" 1>&2
+  echo "Failed: sbt +coveralls" 1>&2
   echo "============================================"
   exit 1
 fi
